f51237bc65d0f2e48f6324465f530b8ab70b43ca
Suyash Joshi <suyashjoshi1504@gmail.com>
2024-11-08 11:36:53 +0530

    DAY 1 AGAIN

---
+++ Data_Structures/Arrays/isNumberPalindrome.cpp
@@ -0,0 +1,40 @@
+/*
+A number is a Palindrome number if the reverse of the number and the numbers itself are equal 
+i.e. if the number and its reverse are the same then a number is a palindrome number.
+
+Example :
+Number : 12321
+Reverse : 12321
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    int reverse=0;
+    int rem;
+    bool isPalindrome;
+    cin>>n;
+    int originalNumber = n;
+
+    while(n>0){
+
+        rem = n%10;
+        reverse = reverse * 10 + rem;
+        n/=10;
+
+    }
+    cout<<reverse<<endl;
+
+    if(originalNumber!=reverse){
+        isPalindrome = false;
+    }
+    else{
+        isPalindrome = true;
+    }
+
+    cout<<isPalindrome<<endl;
+    return 0;
+}
