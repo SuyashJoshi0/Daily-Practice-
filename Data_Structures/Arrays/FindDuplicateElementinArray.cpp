629e8f3f70cc7867b4b0dcb6ba5167c1656ee927
Suyash Joshi <suyashjoshi1504@gmail.com>
2024-11-11 16:11:50 +0530

    DAY 4

--- Data_Structures/Arrays/FindDuplicateElementinArray.cpp
+++ Data_Structures/Arrays/FindDuplicateElementinArray.cpp
@@ -1,15 +1,39 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+*/
+
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+bool isDuplicate(vector<int>arr,int n){
+    sort(arr.begin(),arr.end());
+    for(int i=1;i<n;i++){
+        if(arr[i]==arr[i-1]){
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main(){
 
     int n;
     cin>>n;
-    int arr[n];
+    vector<int>arr(n);
+
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+    cout<<"Duplicate Element is"<<endl;
+    cout<<isDuplicate(arr,n);
     return 0;
 
 }
