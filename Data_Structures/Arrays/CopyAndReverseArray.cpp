64b0ded6e0ebbd567e2fd810aab17374f046accc
Suyash Joshi <suyashjoshi1504@gmail.com>
2024-11-11 15:34:53 +0530

    DAY 3

--- Data_Structures/Arrays/CopyAndReverseArray.cpp
+++ Data_Structures/Arrays/CopyAndReverseArray.cpp
@@ -6,3 +6,38 @@ For example:
 If 'ARR' contains the following elements [ 1, 2, 3, 4, 5 ], then you should return another array that is equal to [ 5, 4, 3, 2, 1].
 
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseArray(vector<int>arr,int n){
+
+    vector<int>tempArray(n);
+
+    for(int i=0;i<n;i++){
+
+        tempArray[i]= arr[n-i-1];   // This logic is also used to swap elements 
+    }                               // in an Array or a String
+
+    for(int i=0;i<n;i++){
+
+        cout<<tempArray[i]<<" "<<endl;
+    }
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"The reversed Array is: "<<endl;
+    reverseArray(arr,n);
+    return 0;
+}
