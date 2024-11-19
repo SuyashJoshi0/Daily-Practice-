8c9127de93cfeb366b6ec8be369be4253aa35954
Suyash Joshi <suyashjoshi1504@gmail.com>
2024-11-19 22:50:55 +0530

    DAY 12

--- Data_Structures/Arrays/SearchInRotatedSortedArray.cpp
+++ Data_Structures/Arrays/SearchInRotatedSortedArray.cpp
@@ -16,26 +16,34 @@ Output: -1
 */
 
 int search(vector<int>& nums, int target) {
-    int start =0;int index = 0;
-    int end = nums.size();
-    int mid = (start + end)/2;
+        int start = 0;
+        int end = nums.size() - 1;
 
-    while(start <= end){
-        if(target == nums[mid]){
-            mid = index;
-        }
-        else if(target < nums[mid]){
-            end--;
-        }
-        else{
-            start++;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            if(nums[start] <= nums[mid]){
+                if(nums[start]<= target && target <= nums[mid]){
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid]<= target && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
             }
         }
-    return index;
+    return -1;
         
-    }
-
-
+}
 
 #include <iostream>
 #include <vector>
