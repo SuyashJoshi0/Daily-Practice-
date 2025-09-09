da24df1cabfa747bbe499e7df5970fb8a40ca321
SuyashJoshi0 <121487530+SuyashJoshi0@users.noreply.github.com>
2025-09-09 22:44:30 +0530

    Lecture 4 all loops

--- Java Notes/Loops.java
+++ Java Notes/Loops.java
@@ -9,3 +9,44 @@ public class Main
 		}
 	}
 }
+
+//WHILE LOOP FOR PRINTING NUMBERS 1 TO 10
+
+public class Main
+{
+	public static void main(String[] args) {
+		int i=0;
+		while(i<11){
+		    System.out.println(i);
+		    i++;
+		}
+	}
+}
+//DO WHILE LOOP FOR PRINTING NUMBERS 1 TO 10
+public class Main
+{
+	public static void main(String[] args) {
+		int i=0;
+		do{
+		  System.out.println(i); 
+		  i++;
+		}
+		while(i<11);
+	}
+}
+
+//SUM OF FIRST NATURAL NUMBERS
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=n;i++){
+		    sum+=i;
+		}
+    System.out.print(sum);
+	}
+}
