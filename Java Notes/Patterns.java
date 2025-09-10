ab43b351930d52bc23e5676969042c82be8a4476
SuyashJoshi0 <121487530+SuyashJoshi0@users.noreply.github.com>
2025-09-10 22:39:37 +0530

    Lecture 5 half done

--- Java Notes/Patterns.java
+++ Java Notes/Patterns.java
@@ -1,10 +1,48 @@
 //SOLID RECTANGLE
 
+import java.util.*;
+
 public class Main
 {
 	public static void main(String[] args) {
-		for(int i=0;i<11;i++){
-		    System.out.println("*");
-		}
+	    Scanner sc = new Scanner(System.in);
+	    int rows = sc.nextInt();
+	    int columns = sc.nextInt();
+	    
+	    for(int i=1;i<=rows;i++){
+	        for(int j=1;j<=columns;j++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}
+
+// OUTPUT for 4 rows and 5 columns
+*****
+*****
+*****
+*****
+
+//HOLLOW RECTANGLE
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int rows = sc.nextInt();
+	    int columns = sc.nextInt();
+	    
+	    for(int i=1;i<=rows;i++){
+	        for(int j=1;j<=columns;j++){
+	            if(i==1 || j==1 || i==rows || j==columns){
+	                 System.out.print("*");
+	            }else{
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.println();
+	    }
 	}
 }
