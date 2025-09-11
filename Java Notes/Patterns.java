ce96910ac67e4bd88b153d306bdaa07509b239ba
SuyashJoshi0 <121487530+SuyashJoshi0@users.noreply.github.com>
2025-09-12 00:09:07 +0530

    Update Patterns.java

--- Java Notes/Patterns.java
+++ Java Notes/Patterns.java
@@ -46,3 +46,58 @@ public class Main
 	    }
 	}
 }
+//OUTPUT for 4 rows and 5 colums 
+*****
+*   *
+*   *
+*****
+
+// HALF PYRAMID
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	   // int rows = sc.nextInt();
+	   // int columns = sc.nextInt();
+	   int n = sc.nextInt();
+	    
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=i;j++){
+	           System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}
+
+// OUTPUT for n = 4
+*
+**
+***
+****
+
+//HALF INVERTED PYRAMID
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    
+	    for(int i=n;i>=1;i--){
+	        for(int j=1;j<=i;j++){
+	           System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}
+
+//OUTPUT for n = 4
+****
+***
+**
+*
